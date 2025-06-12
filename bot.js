6f2cfa442496b2177e8e1ae0fb439ac533ab383e
JoaoPedroos <joaopedroenv@gmail.com>
2025-06-12 02:03:10 -0300

    Atualização do Bot

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({
   host: process.env.MC_HOST,
   port: parseInt(process.env.MC_PORT),
-  username: process.env.MC_USERNAME || 'MeuBotAFK',
-  auth: 'offline', // IMPORTANTE para servidores pirata
-  version: false,  // auto detecta a versão do servidor
+  username: process.env.MC_USERNAME,
+  auth: 'offline',
+  version: false
 })
